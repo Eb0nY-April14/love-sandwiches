744365a5956b84d19f5c04119904030254bc91c0
Eb0nY-April14 <laratemi14@gmail.com>
2021-07-16 09:56:27 +0000

    Fix trailing whitespace and long character warnings

--- run.py
+++ run.py
@@ -1,1 +1,34 @@
 # python code goes here
+import gspread
+from google.oauth2.service_account import Credentials
+
+# All the constants
+# written below from
+# line 6 to 15 of our
+# code are the settings
+# we need in order to
+# access our spreadsheet data.
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches-MS3')
+
+# Here, we declare a
+# 'sales' variable and
+# use the worksheet method
+# of the sheet to call
+# our "sales" worksheet.
+sales = SHEET.worksheet('sales')
+
+# Here, we declare another variable called 'data' and use the get_all_values()
+# of the gspread method to pull all the values from our sales worksheet.
+data = sales.get_all_values()
+
+# This prints the content of the 'data' variable to the terminal.
+print(data)
